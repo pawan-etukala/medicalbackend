e6a90791735655d9550693a784cb3c46171e089f
Your Name <pawanetukala@gmail.com>
2024-10-07 15:16:09 +0530

    commit

--- src/main/java/com/medical/repository/ChapterRepository.java
+++ src/main/java/com/medical/repository/ChapterRepository.java
@@ -8,11 +8,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.medical.entity.Chapter;
 
 public interface ChapterRepository extends JpaRepository<Chapter, Long> {
-<<<<<<< HEAD
-	Optional<Chapter> findByid(Long chapterNumber);
-=======
+
 
 	Optional<Chapter> findByChapterNumber(Long chapterNumber);
 //	findByChapterNumber(long chapterNumber);
->>>>>>> b28335fff69b297a6ac91ca4205f735299863f2e
+
 }
